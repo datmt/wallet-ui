eb250d891d442f899c6c86d5648af9007f2758aa
Mạnh Đạt <dat.tm24@gmail.com>
2023-10-21 11:13:04 +0700

    cosmetic

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from "@auth0/auth0-angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
 
+    constructor(private authService: AuthService) {
+    }
+
+    isAuthenticated() {
+        return this.authService.isAuthenticated$;
+    }
+
+    signIn() {
+        this.authService.loginWithRedirect();
+    }
+    signOut() {
+        this.authService.logout();
+    }
 }
